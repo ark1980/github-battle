56cbc127db258f63b05eefe3c083f36b7ba2d2ec
Ali <ar.k@outlook.com>
2018-01-26 13:02:42 -0800

    add Switch to Router and create NotMatch component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Popular from './components/Popular';
 import Nav from './components/Nav';
 import Home from './components/Home';
 import Battle from './components/Battle';
+import NotMatch from './components/NotMatch';
 
 import {
   BrowserRouter as Router,
   Route,
+  Switch
 } from 'react-router-dom';
 
 
@@ -21,9 +23,12 @@ class App extends Component {
             <h1 className="app-title">Welcome to Github-Battle</h1>
             <Nav />
           </header>
-          <Route exact path='/' component={Home}/>
-          <Route path='/battle' component={Battle}/>
-          <Route exact path='/popular' component={Popular}/>
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route path='/battle' component={Battle}/>
+            <Route exact path='/popular' component={Popular}/>
+            <Route component={NotMatch} />
+          </Switch>
         </div>
       </Router>
     );
---
+++ src/components/NotMatch.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotMatch = () => {
+  return (
+    <div>
+      <h1>The Page You're Looknig for Not Found</h1>
+      <img src="https://cdn.mos.cms.futurecdn.net/9vkE5D4ytKM8So2A75Ekbk-970-80.jpg" alt="Not Match"/>
+      <h3>Error 404</h3>
+    </div>
+  )
+}
+
+export default NotMatch;
