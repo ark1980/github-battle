94195f117e0888e34dce7cb061fe16cef444ad5d
Ali <ar.k@outlook.com>
2018-01-31 11:21:58 -0800

    add Battle button into Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div className="home-container">
       <h2>Github Battle: Battle your friends... and stuff.</h2>
+      <Link to="/battle" className="button" >
+        Battle
+      </Link>
     </div>
   )
 }
